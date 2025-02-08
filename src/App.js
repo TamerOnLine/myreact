48b317dcf4f286717eab2ae32a693749ab5aa7b1
TamerOnLine <tamer@tameronline.com>
2025-02-09 00:07:00 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>My New React Project!</h1>
+      <p>Start customizing your project now!</p>
+    </div>
+  );
+}
+
+export default App;
